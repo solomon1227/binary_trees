2f13f417fd7fad7494c025760524b960d020390e
solomon1227 <solomonf1227@gmail.com>
2023-02-28 11:54:35 +0300

    Create a node in binary tree

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,15 +7,12 @@
   */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	if (parent == NULL)
-		return (NULL);
-
 	binary_tree_t *new = malloc(sizeof(binary_tree_t));
 
 	if (new == NULL)
 		return (NULL);
 
-	new.value = value;
+	new->n = value;
 	new->parent = parent;
 	new->right = NULL;
 	new->left = NULL;
