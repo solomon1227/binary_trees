23277d1e03c920ee866a10e23cf850c7f74f3cac
solomon1227 <solomonf1227@gmail.com>
2023-03-01 15:02:22 +0300

    balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -16,7 +16,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		if (tree)
 		{
 			l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
-			r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+			r = tree->right ? 1 + binary_tree_height(tree->right)
+				: 1;
 		}
 		return ((l > r) ? l : r);
 	}
