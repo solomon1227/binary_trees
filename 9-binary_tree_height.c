775f8ad2450cbbc23ecb48fd46f80f3c5d7246b0
solomon1227 <solomonf1227@gmail.com>
2023-03-01 07:37:40 +0300

    height of a binary tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -5,13 +5,20 @@
   * Return: height of a tree
   */
 size_t binary_tree_height(const binary_tree_t *tree)
-{
-	int left_height, right_height;
+{	size_t l = 0;
+	size_t r = 0;
 
 	if (tree == NULL)
-		return (-1);
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	return (((left_height > right_height) ? left_height : right_height) + 1);
+	{
+		return (0);
+	}
+	else
+	{
+		if (tree)
+		{
+			l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+			r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		}
+		return ((l > r) ? l : r);
+	}
 }
